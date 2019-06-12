4d0a77edc015bf8e84eed0fc1d2b10b6b176293c
Nntraveler <nntraveler@foxmail.com>
2019-06-13 00:07:44 +0800

    fix(BaseCharacter): fix a bug cause potential compile error

--- Source/OurMoba/BaseCharacter.cpp
+++ Source/OurMoba/BaseCharacter.cpp
@@ -10,7 +10,6 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/SpringArmComponent.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Materials/Material.h"
 #include "Engine/World.h"
 #include "MobaController.h"
