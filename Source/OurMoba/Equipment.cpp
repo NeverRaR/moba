88fb655918fcbc5a15ee78907bf6261f73f7474e
Nntraveler <nntraveler@foxmail.com>
2019-06-14 15:48:27 +0800

    fix(Equipment): allow Equipment affecting client in multiplay

--- Source/OurMoba/Equipment.cpp
+++ Source/OurMoba/Equipment.cpp
@@ -62,6 +62,254 @@ void UEquipment::RemoveEquipment(ABaseEquipment*NewEquipment)
 		AllEquipment.Remove(NewEquipment);
 		NewEquipment->Destroy();
 	}
-
 }
 
+void UEquipment::ServerModifyGold_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddMoney(Amount);
+}
+
+bool UEquipment::ServerModifyGold_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurHP_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurHP(Amount);
+}
+
+bool UEquipment::ServerModifyCurHP_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurHPRecovery_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurHPRecovery(Amount);
+}
+
+bool UEquipment::ServerModifyCurHPRecovery_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurMP_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurMP(Amount);
+}
+
+bool UEquipment::ServerModifyCurMP_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurMPRecovery_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurMPRecovery(Amount);
+}
+
+bool UEquipment::ServerModifyCurMPRecovery_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurPhyAttack_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurPhyAttack(Amount);
+}
+
+bool UEquipment::ServerModifyCurPhyAttack_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurPhyDef_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurPhyDef(Amount);
+}
+
+bool UEquipment::ServerModifyCurPhyDef_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurMagAttack_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurMagAttack(Amount);
+}
+
+bool UEquipment::ServerModifyCurMagAttack_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurMagDef_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurMagDef(Amount);
+}
+
+bool UEquipment::ServerModifyCurMagDef_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurAttackSpeed_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurAttackSpeed(Amount);
+}
+
+bool UEquipment::ServerModifyCurAttackSpeed_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurMoveSpeed_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurMoveSpeed(Amount);
+}
+
+bool UEquipment::ServerModifyCurMoveSpeed_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurLeech_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurLeech(Amount);
+}
+
+bool UEquipment::ServerModifyCurLeech_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyCurCDReduction_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddCurCDReduction(Amount);
+}
+
+bool UEquipment::ServerModifyCurCDReduction_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMaxHP_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMaxHP(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMaxHP_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseHPRecovery_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseHPRecovery(Amount);
+}
+
+bool UEquipment::ServerModifyBaseHPRecovery_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMaxMP_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMaxMP(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMaxMP_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMPRecovery_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMPRecovery(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMPRecovery_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBasePhyAttack_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBasePhyAttack(Amount);
+}
+
+bool UEquipment::ServerModifyBasePhyAttack_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBasePhyDef_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBasePhyDef(Amount);
+}
+
+bool UEquipment::ServerModifyBasePhyDef_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMagAttack_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMagAttack(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMagAttack_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMagDef_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMagDef(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMagDef_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseAttackSpeed_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseAttackSpeed(Amount);
+}
+
+bool UEquipment::ServerModifyBaseAttackSpeed_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseMoveSpeed_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseMoveSpeed(Amount);
+}
+
+bool UEquipment::ServerModifyBaseMoveSpeed_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseLeech_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseLeech(Amount);
+}
+
+bool UEquipment::ServerModifyBaseLeech_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
+
+void UEquipment::ServerModifyBaseCDReduction_Implementation(float Amount, AHero * TargetPawn)
+{
+	TargetPawn->PropertyComp->AddBaseCDReduction(Amount);
+}
+
+bool UEquipment::ServerModifyBaseCDReduction_Validate(float Amount, AHero * TargetPawn)
+{
+	return true;
+}
