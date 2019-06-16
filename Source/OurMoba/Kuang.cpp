d9705cbadb5b73b2605dae8171067ebff03638b6
Nntraveler <nntraveler@foxmail.com>
2019-06-16 13:00:34 +0800

    feat(Kuang): modify Kuang's property

--- Source/OurMoba/Kuang.cpp
+++ Source/OurMoba/Kuang.cpp
@@ -116,7 +116,7 @@ void AKuang::Skill2LightDurance(FVector Target)
 	for (int32 i = 0; i < AllEnemysInRadius.Num(); ++i)
 	{
 		ADurance* Durance = GetWorld()->SpawnActor<ADurance>(ADurance::StaticClass());
-		Durance->SustainTime = 1.5 + SkillComp->GetSkillLevel(1)*0.25f;
+		Durance->SustainTime = 1.2 + SkillComp->GetSkillLevel(1)*0.2f;
 		AllEnemysInRadius[i]->BuffComp->AddBuff(Durance);
 		AllEnemysInRadius[i]->ReceiveMagDamage(Damage, this);
 	}
@@ -127,9 +127,9 @@ void AKuang::Skill3Frenzy()
 	if (!SkillComp->CheckCanBeReleased(2)) return;
 	SkillComp->ReleaseSkill(2);
 	ARage* Rage = GetWorld()->SpawnActor<ARage>(ARage::StaticClass());
-	Rage->SustainTime = 7.0f + SkillComp->GetSkillLevel(2)*2.0f;
-	Rage->DeltaAttackSpeed= (0.1 + SkillComp->GetSkillLevel(2)*0.05)*PropertyComp->GetCurAttackSpeed();
-	Rage->DeltaPhyDamage= (0.1 + SkillComp->GetSkillLevel(2)*0.05)*PropertyComp->GetCurPhyAttack();
+	Rage->SustainTime = 7.0f + SkillComp->GetSkillLevel(2)*1.0f;
+	Rage->DeltaAttackSpeed= (0.2 + SkillComp->GetSkillLevel(2)*0.05)*PropertyComp->GetCurAttackSpeed();
+	Rage->DeltaPhyDamage= (0.2 + SkillComp->GetSkillLevel(2)*0.05)*PropertyComp->GetCurPhyAttack();
 	BuffComp->AddBuff(Rage);
 }
 
